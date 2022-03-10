f63bfce6be0eb2aab3fa9c052ea1db4f6ae52f0f
Rohrbacher Marcell <66619538+Marci971104@users.noreply.github.com>
2022-03-10 11:07:03 +0100

    fejléc

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jarmu'`, () => {
+  it(`should have as title 'angular_doga'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('jarmu');
